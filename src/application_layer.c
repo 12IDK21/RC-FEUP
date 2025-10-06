2f6a28b9fc847930ea63433364b232f29316d840
ArthurTeixeira16 <arthur.mello.teixeira@gmail.com>
2025-10-06 15:57:34 +0100

    Update application_layer.c

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,11 +1,16 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
-
+#include "link_layer.h"
 #include <stdio.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+    LinkLayer ll;
+    strcpy(serialPort,ll.serialPort);
+    if(role == "tx") ll.role ? 0 : 1;
+    
+    llopen(ll);
 }
+
